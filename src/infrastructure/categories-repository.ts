304117e7e0eea508f2180892ca124ffdfea7fadc
GenesisMRL <nunomiglima@gmail.com>
2022-06-09 10:33:20 +0100

    removed try catch from categories infrastructure

--- src/infrastructure/categories-repository.ts
+++ src/infrastructure/categories-repository.ts
@@ -4,26 +4,23 @@ const prisma = new PrismaClient();
 
 export const getCategoryById = async (id?: string) => {
   if (isNaN(Number(id))) {
-    try {
-        const category = await prisma.category.findUnique({
-          where: {
-            name: id,
-          },
-        });
-        return category === null ? "Category not found" : category;
-      } catch (e) {
-        console.log(e);
-      }
-  }
-  try {
     const category = await prisma.category.findUnique({
       where: {
-        id: Number(id),
+        name: id,
       },
     });
-    return category === null ? "Category not found" : category;
-  } catch (e) {
-    console.log(e);
+    if (category === null) {
+      return category;
+    }
+    throw new Error("Category not found");
   }
+  const category = await prisma.category.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  if (category === null) {
+    return category;
+  }
+  throw new Error("Category not found");
 };
-
