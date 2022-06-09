d5feb34a2a96aac2bc40d154567c3ddd22b1c5d0
GenesisMRL <nunomiglima@gmail.com>
2022-06-09 13:10:05 +0100

    improved get subcats by main cat endpoint

--- src/infrastructure/categories-repository.ts
+++ src/infrastructure/categories-repository.ts
@@ -3,14 +3,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getCategoryByMainCat = async (mainCat?: string) => {
-  const categories = await prisma.category.findMany({
+  const main = await prisma.category.findUnique({
+    where: {
+      name: mainCat,
+    },
+  });
+  const subcategories = await prisma.category.findMany({
     where: {
       upperLevel: mainCat,
     },
   });
-  if (categories !== null) {
-    return categories;
-  }
-  throw new Error("Category not found");
+  return { main: main, subcategories };
 };
-
