cac7964507060c3923d4362b595c612de45c569f
Tiago SIlva <tigaonline12@gmail.com>
2022-06-13 11:03:16 +0100

    cloud i guess

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello!");
 });
 
-app.listen(port, "127.0.0.1", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`listening on port ${port} :)`);
 });
 
