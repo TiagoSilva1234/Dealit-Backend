44b0f1c51bd03bc671884f7af9d0b3a2a468dcf0
GenesisMRL <nunomiglima@gmail.com>
2022-06-14 10:17:21 +0100

    prisma model change

--- src/application/index.ts
+++ src/application/index.ts
@@ -16,43 +16,43 @@ import { Express } from "express";
 
 //EndpointsUser
 export const endpointGetUserById = (app: Express) =>
-  app.get("dealit/api/users/:id", getUserById);
+  app.get("/dealit/api/users/:id", getUserById);
 
 //EndpointsAuth
 export const endpointPostUser = (app: Express) =>
-  app.post("dealit/api/register", registerUser);
+  app.post("/dealit/api/register", registerUser);
 
 export const endpointPostLogin = (app: Express) =>
-  app.post("dealit/api/login", userLogin);
+  app.post("/dealit/api/login", userLogin);
 
 //EndpointsProduct
 export const endpointGetProductById = (app: Express) =>
-  app.get("dealit/api/products/:id", getProductById);
+  app.get("/dealit/api/products/:id", getProductById);
 
 export const endpointPostProduct = (app: Express) =>
-  app.post("dealit/api/products", verifyToken, postNewProduct);
+  app.post("/dealit/api/products", verifyToken, postNewProduct);
 
 export const endpointgetProductsByCategoryPaginated = (app: Express) =>
   app.get(
-    "dealit/api/products/category/:category",
+    "/dealit/api/products/category/:category",
     getProductsByCategory
   );
 
 export const endpointgetAllProductsPaginated = (app: Express) =>
-  app.get("dealit/api/products/", getAllProductsPaginated);
+  app.get("/dealit/api/products/", getAllProductsPaginated);
 
 export const endpointgetProductsByUserId = (app: Express) =>
-  app.get("dealit/api/products/user/:userId", getProductsByUserId);
+  app.get("/dealit/api/products/user/:userId", getProductsByUserId);
 
 export const endpointGetLatestProducts = (app: Express) =>
-  app.get("dealit/api/latest-products", getLateProducts);
+  app.get("/dealit/api/latest-products", getLateProducts);
 //EndpointsCategory
 export const endpointGetCategoryByMainCat = (app: Express) =>
-  app.get("dealit/api/categories/:mainCat", getCategoryByMainCat);
+  app.get("/dealit/api/categories/:mainCat", getCategoryByMainCat);
 
 export const endpointGetAllMainCategories = (app: Express) =>
-  app.get("dealit/api/categories", getAllMainCategories);
+  app.get("/dealit/api/categories", getAllMainCategories);
 
 //EndpointsOrders
 export const endpointGetOrdersByUserId = (app: Express) =>
-  app.get("dealit/api/orders/:userId", getOrdersByUserId);
+  app.get("/dealit/api/orders/:userId", getOrdersByUserId);
