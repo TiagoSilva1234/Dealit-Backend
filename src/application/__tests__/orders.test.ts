829bae5eb3ce0e56710c62e65355ba9652bf8591
GenesisMRL <nunomiglima@gmail.com>
2022-06-22 16:05:05 +0100

    added creditCards and addresses tests

--- src/application/__tests__/orders.test.ts
+++
@@ -1,6 +0,0 @@
-//const {}
-
-const  getUserOrders =require( "../domain/orders/get-ordersByUserId")
-const  postOrders = require("../domain/orders/post-order")
-const  patchOrderSendDate =require( "../domain/orders/patch-orderSendDate")
-const  patchOrderDeliveryDate = require( "../domain/orders/patch-orderDeliveryDate")
